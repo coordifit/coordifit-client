0966dce0d17c4a40835afa03ada07daebfebecb1
kyeongjun-ko <rhrudwnszoq@naver.com>
2025-09-19 14:10:40 +0900

    chore: setup CI/CD pipeline using git actions and s3

--- src/shared/api/axiosInstance.js
+++ src/shared/api/axiosInstance.js
@@ -1,5 +1,6 @@
 import axios from "axios";
+import { API_URL } from "../config/config";
 
-export const api = axios.create({
-  baseURL: "http://localhost:8080/api",
-});
+const api = axios.create({ baseURL: API_URL });
+
+export { api };

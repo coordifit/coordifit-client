16ca62b1d82804630ac699183ee2b2834918fc98
kyeongjun-ko <rhrudwnszoq@naver.com>
2025-09-23 17:48:16 +0900

    feat: add mobile size layout component

---
+++ src/pages/Closet/Closet.jsx
@@ -0,0 +1,7 @@
+import styles from "./Closet.module.css";
+
+const Closet = () => {
+  return <h3 className={styles.color}>Closet</h3>;
+};
+
+export default Closet;
---
+++ src/pages/Snap/Snap.jsx
@@ -0,0 +1,7 @@
+import styles from "./Snap.module.css";
+
+const Snap = () => {
+  return <h3 className={styles.color}>Snap</h3>;
+};
+
+export default Snap;

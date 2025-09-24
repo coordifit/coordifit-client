bb25eb77300b02060d95aa8903e96a91ae55b2ff
kyeongjun-ko <rhrudwnszoq@naver.com>
2025-09-24 16:26:48 +0900

    feat: add calendar layout and daily/monthly view toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "@/components/Layout/Layout";
-import CalendarPage from "@/pages/CalendarPage/CalendarPage";
+import CalendarPage from "@/pages/Calendar/CalendarPage/CalendarPage";
 import ClosetPage from "@/pages/ClosetPage/ClosetPage";
 import LoginPage from "@/pages/LoginPage/LoginPage";
 import MainPage from "@/pages/MainPage/MainPage";
@@ -10,26 +10,30 @@ import SignUpPage from "@/pages/SignUpPage/SignUpPage";
 import SnapPage from "@/pages/SnapPage/SnapPage";
 import Start from "@/pages/Start/Start";
 import MyPage from "@/pages/MyPage/MyPage";
-import ProfileEditPage from "./pages/ProfileEditPage.jsx/ProfileEditPage";
+import ProfileEditPage from "@/pages/ProfileEditPage.jsx/ProfileEditPage";
+import CalendarRouter from "./pages/Calendar/CalendarRouter";
+
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Start />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/signup" element={<SignUpPage />} />
-                <Route path="/password-reset" element={<PasswordReset />} />
-                <Route element={<Layout />}>
-                    <Route path="/main" element={<MainPage />} />
-                    <Route path="/calendar" element={<CalendarPage />} />
-                    <Route path="/snap" element={<SnapPage />} />
-                    <Route path="/closet" element={<ClosetPage />} />
-                    <Route path="/mypage" element={<MyPage />} />
-                    <Route path="/profile/edit" element={<ProfileEditPage />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/password-reset" element={<PasswordReset />} />
+        <Route element={<Layout />}>
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/calendar" element={<CalendarPage />}>
+            <Route path=":date" element={<CalendarRouter />} />
+          </Route>
+          <Route path="/snap" element={<SnapPage />} />
+          <Route path="/closet" element={<ClosetPage />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/profile/edit" element={<ProfileEditPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/Calendar/CalendarPage/CalendarPage.jsx
@@ -0,0 +1,141 @@
+import { Outlet, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+import Calendar from "react-calendar";
+import classNames from "classnames/bind";
+
+import Modal from "@/components/Modal/Modal";
+// import DatePicker from "../DatePicker/DatePicker";
+
+import "react-calendar/dist/Calendar.css";
+import styles from "./CalendarPage.module.css";
+
+const cx = classNames.bind(styles);
+
+const CalendarPage = () => {
+  const navigate = useNavigate();
+  const [targetDate, setTargetDate] = useState(new Date());
+  const [viewMode, setViewMode] = useState("monthly");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toDateString = (dateObject) => dateObject.toISOString().split("T")[0];
+  const toYearMonthString = (dateObject) => dateObject.toISOString().slice(0, 7);
+
+  const clickHandler = (date) => {
+    const dateString = toDateString(date);
+
+    setTargetDate(date);
+    setViewMode("daily");
+    navigate(`/calendar/${dateString}`);
+  };
+
+  const handleDateMove = (type) => {
+    console.log("targetDate", targetDate);
+    const moveTargetDate = new Date(targetDate);
+    console.log("moveTargetDate", moveTargetDate);
+
+    if (type === "prev") {
+      moveTargetDate.setDate(targetDate.getDate() - 1);
+    } else if (type === "next") {
+      moveTargetDate.setDate(targetDate.getDate() + 1);
+    }
+
+    setTargetDate(moveTargetDate);
+    navigate(`/calendar/${toDateString(moveTargetDate)}`);
+  };
+
+  const handleViewMode = (e) => {
+    const viewMode = e.currentTarget.innerText;
+
+    if (viewMode === "monthly") {
+      const yearMonth = toYearMonthString(targetDate);
+
+      navigate(`/calendar/${yearMonth}`);
+    } else if (viewMode === "daily") {
+      const dateString = toDateString(targetDate);
+
+      navigate(`/calendar/${dateString}`);
+    }
+
+    setViewMode(viewMode);
+  };
+
+  const handleModal = (type) => {
+    if (type === "open") {
+      setIsModalOpen(true);
+    } else if (type === "close") {
+      setIsModalOpen(false);
+    }
+  };
+
+  // const handleDatePicker = (date) => {
+  //   console.log("date.month", date.month);
+  //   const pickedDate = `${date.year}-${date.month}-${date.day}`;
+  //   const pickedDateObject = new Date(pickedDate);
+
+  //   setIsModalOpen(false);
+  //   setTargetDate(pickedDateObject);
+  //   setViewMode("daily");
+  //   navigate(`/calendar/${pickedDate}`);
+  // };
+
+  const ModalContent = (
+    <ul>
+      {Array.from({ length: 31 }, (_, i) => i + 1).map((day) => (
+        <li key={day}>{day}일</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <>
+      <div className={cx("viewmode-box")}>
+        <div
+          onClick={handleViewMode}
+          className={cx("viewmode", { active: viewMode === "daily" })}
+        >
+          daily
+        </div>
+        <div
+          onClick={handleViewMode}
+          className={cx("viewmode", { active: viewMode === "monthly" })}
+        >
+          monthly
+        </div>
+      </div>
+      {viewMode === "monthly" && (
+        <>
+          <span>
+            {targetDate.getFullYear()}년 {targetDate.getMonth() + 1}월
+          </span>
+          <button onClick={() => handleModal("open")}>날짜 선택 ▼</button>
+
+          <Calendar
+            onChange={setTargetDate}
+            value={targetDate}
+            onClickDay={clickHandler}
+          />
+          {isModalOpen && (
+            <Modal
+              title="날짜선택"
+              onClose={() => handleModal("close")}
+              children={ModalContent}
+            >
+              {/* <DatePicker onConfirm={handleDatePicker} /> */}
+            </Modal>
+          )}
+        </>
+      )}
+      {viewMode === "daily" && (
+        <div>
+          <button onClick={() => handleDateMove("prev")}>prev</button>
+          {targetDate.toISOString()}
+          <button onClick={() => handleDateMove("next")}>next</button>
+        </div>
+      )}
+      <Outlet />
+    </>
+  );
+};
+
+export default CalendarPage;

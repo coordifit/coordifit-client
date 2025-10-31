5e05b2d50f1b87d4139489af4b358eac659a3283
Geonho Kim <yorjsgh@naver.com>
2025-10-31 12:07:46 +0900

    feat : 옷삭제시 코디 적용

--- src/pages/ClosetPage/ClosetDetailPage.jsx
+++ src/pages/ClosetPage/ClosetDetailPage.jsx
@@ -272,6 +272,7 @@ const ClosetDetailPage = () => {
 
       if (response.success) {
         alert("옷이 삭제되었습니다.");
+        queryClient.invalidateQueries(["coordis"]);
         navigate("/closet");
       } else {
         alert("삭제에 실패했습니다: " + response.message);
